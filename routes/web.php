ddc6d809db6698039354d5a5b5aee98abc575f9b
ehsanmim <115899665+ehsanmim@users.noreply.github.com>
2025-07-06 12:05:30 +0200

    Configures dynamic database connections
    Sets up dynamic database connections based on environment variables
    for exporting data, allowing to connect to different databases.
    
    This setup enables to retrieve table names from the specified export database.
    
    Adds environment variables to .env.example file.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 
 Route::get('/', function () {
@@ -8,8 +10,32 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
+
+    $initialDbConfig = config('database');
+    
+    $exportDbConnection = env('EXPORT_DB_CONNECTION');
+    config([
+        "database.connections.{$exportDbConnection}.host"     => env('EXPORT_DB_HOST'),
+        "database.connections.{$exportDbConnection}.port"     => env('EXPORT_DB_PORT'),
+        "database.connections.{$exportDbConnection}.username" => env('EXPORT_DB_USERNAME'),
+        "database.connections.{$exportDbConnection}.password" => env('EXPORT_DB_PASSWORD'),
+        "database.connections.{$exportDbConnection}.database" => env('EXPORT_DB_DATABASE'),
+        'database.default'                                   => $exportDbConnection,
+    ]);
+    $tables = Schema::getTables(env('EXPORT_DB_DATABASE'));
+    $exportTableNames = array_column($tables, 'name');
+
+
+    dump($exportTableNames);
+
+
+    // set back to initial database configuration
+    config(['database' => $initialDbConfig]);
+
+    Route::get('dashboard', function ()  use ($exportTableNames) {
+        return Inertia::render('dashboard', [
+            'exportTableNames' => $exportTableNames,
+        ]);
     })->name('dashboard');
 });
 
